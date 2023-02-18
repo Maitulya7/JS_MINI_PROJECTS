62a85e306e92326528214f24701b067ef334b62b
Maitulya Vaghela <maitulya7@gmail.com>
2023-02-18 10:58:28 +0530

    PROJECT-1
    DRUM-KIT

---
+++ drum-kit/app.js
@@ -0,0 +1,21 @@
+
+  window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if (!audio) return;
+    
+    audio.currentTime = 0;
+    audio.play();
+    
+    key.classList.add('playing');
+  });
+  
+  window.addEventListener('keyup', function(e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if (!key) return;
+    
+    key.classList.remove('playing');
+  });
+  

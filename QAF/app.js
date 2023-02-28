f102bfcad6a9e54b63c929d550d4bf0d249ed0cd
Maitulya Vaghela <maitulya7@gmail.com>
2023-02-28 16:49:32 +0530

    project-11

--- QAF/app.js
+++ QAF/app.js
@@ -3,6 +3,6 @@ const container = document.getElementsByClassName("content-container")
 for ( i = 0 ; i < container.length ; i++){
     container[i].addEventListener("click", function(){
         this.classList.toggle('active')
-        console.log("hello")
+
     })
 }

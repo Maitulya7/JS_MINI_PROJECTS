106cfe8e9802f75ffdc657e8755fbf3c441dac07
Maitulya Vaghela <maitulya7@gmail.com>
2023-03-25 14:16:48 +0530

    React_project_4

---
+++ React_project_4/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Cards from "../components/Cards";
+
+export default function App(){
+  return(
+    <div>
+      <Navbar />
+      < Hero />
+      < Cards />
+    </div>
+    
+  )
+}
